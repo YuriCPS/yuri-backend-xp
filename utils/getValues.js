d5c8faf6195235ee1c3821252d7a5cd21d36834a
Yuri Carvalho <yuricps@gmail.com>
2022-07-21 23:10:56 -0300

    refactor: refatora a funçao utilitaria getValues pra considerar apenas os ativos com quantidade maior que 0

--- utils/getValues.js
+++ utils/getValues.js
@@ -1,15 +1,17 @@
 module.exports = (clientAssets, allAssets) => {
   const assetsWithValues = [];
 
-  clientAssets.map(clientAsset => {
-    assetsWithValues.push(
-      {
-        codCliente: clientAsset.codCliente,
-        codAtivo: clientAsset.codAtivo,
-        qtdeAtivo: clientAsset.qtdeAtivo,
-        valor: allAssets.find(asset => asset.codAtivo === clientAsset.codAtivo).valor,
-      }
-    );
+  clientAssets.forEach(clientAsset => {
+    if (clientAsset.qtdeAtivo !== 0) {
+      assetsWithValues.push(
+        {
+          codCliente: clientAsset.codCliente,
+          codAtivo: clientAsset.codAtivo,
+          qtdeAtivo: clientAsset.qtdeAtivo,
+          valor: allAssets.find(asset => asset.codAtivo === clientAsset.codAtivo).valor,
+        }
+      );
+    }
   });
 
   return assetsWithValues;
